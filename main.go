7cfe781dd54c6c4d4df9d8f713194dff96b07b29
Calvin Atlan <calvinatlan@gmail.com>
2021-12-27 23:18:58 -0800

    Get id logging working in the db

--- main.go
+++ main.go
@@ -1,19 +1,79 @@
 package main
 
 import (
-	"io/ioutil"
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/mattn/go-sqlite3"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	db := createDB()
+	createTable(db)
+	setUpRouter(db)
+}
 
-	id := "29449073"
+func createDB() *sql.DB {
+	if _, err := os.Stat("dota-rank-performance.db"); os.IsNotExist(err) {
+		file, err := os.Create("dota-rank-performance.db")
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		file.Close()
+	}
+
+	db, _ := sql.Open("sqlite3", "dota-rank-performance.db")
+	return db
+}
+
+func createTable(db *sql.DB) {
+	createPlayerTableSQL := `
+	CREATE TABLE IF NOT EXISTS player (
+		"idPlayer" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"createdDate" DATE
+	);`
+
+	stmt, err := db.Prepare(createPlayerTableSQL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	stmt.Exec()
+}
+
+func insertPlayer(db *sql.DB, id string) {
+	//insertPlayerSQL := `INSERT INTO player(idPlayer, createdDate) VALUES (?, ?);`
+	insertPlayerSQL := `INSERT INTO player(idPlayer, createdDate) VALUES (?, ?);`
+	stmt, err := db.Prepare(insertPlayerSQL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	stmt.Exec(id, time.Now())
+	log.Print("Inserted player id " + id)
+}
+
+func getPlayers(db *sql.DB) string {
+	getPlayersSQL := `SELECT idPlayer FROM player;`
+	rows, err := db.Query(getPlayersSQL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var id int
+	var ids []string
+	for rows.Next() {
+		rows.Scan(&id)
+		log.Print(id)
+		ids = append(ids, strconv.Itoa(id))
+	}
+	return strings.Join(ids, " ")
+}
+
+func setUpRouter(db *sql.DB) {
+	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -28,8 +88,38 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
-	router.POST("/refresh-games", func(c *gin.Context) {
-		resp, err := http.Get("https://api.opendota.com/api/players/" + id + "/peers")
+	router.GET("/get-players", getPlayersHandler(db))
+
+	router.POST("/refresh-games", postRefreshGamesHandler(db))
+
+	router.Run(":" + port)
+}
+
+func getPlayersHandler(db *sql.DB) gin.HandlerFunc {
+	fn := func(c *gin.Context) {
+		players := getPlayers(db)
+		log.Print(players)
+		c.String(http.StatusOK, players)
+	}
+	return gin.HandlerFunc(fn)
+}
+
+func postRefreshGamesHandler(db *sql.DB) gin.HandlerFunc {
+	fn := func(c *gin.Context) {
+
+		type RefreshGameBody struct {
+			PlayerId string `json:"playerId"`
+		}
+		var requestBody RefreshGameBody
+		err := c.BindJSON(&requestBody)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		id := requestBody.PlayerId
+
+		insertPlayer(db, id)
+
+		/*resp, err := http.Get("https://api.opendota.com/api/players/" + id + "/peers")
 		if err != nil {
 			log.Fatalln(err)
 			c.String(http.StatusInternalServerError, "")
@@ -39,8 +129,10 @@ func main() {
 			log.Fatalln(err)
 			c.String(http.StatusInternalServerError, "")
 		}
-		c.String(http.StatusOK, string(body))
-	})
+		//c.String(http.StatusOK, string(body))
+		*/
+		c.String(http.StatusOK, string("Refreshed player "+id))
 
-	router.Run(":" + port)
+	}
+	return gin.HandlerFunc(fn)
 }
--- static/main.js
+++ static/main.js
@@ -1,12 +1,32 @@
-const refreshGames = () => {
+const refreshGames = (event) => {
+    event.preventDefault();
     const url = "/refresh-games";
+    console.log(event.target.elements.playerId.value);
     fetch(url, {
-        method: 'POST'
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            playerId: event.target.elements.playerId.value
+        })
     }).then(res => {
         return res.text()
     }).then(body => {
         const refreshResponseP = document.querySelector('#refresh-response');
         refreshResponseP.innerHTML = body;
         refreshResponseP.style.display = "block";
+    });
+}
+
+const form = document.getElementById('refresh-games-form');
+form.addEventListener('submit', refreshGames);
+
+const getPlayers = () => {
+    const url = "/get-players";
+    fetch(url, {
+        method: 'GET'
+    }).then(res => {
+        return res.text()
+    }).then(body => {
+        console.log(body);
     });
 }
